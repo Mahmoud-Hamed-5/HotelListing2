a553610a427ef215013545aed0871f449943cd7c
Mahmoud-Hamed-5 <mahmoud.hamed91.mh@gmail.com>
2022-06-16 00:22:20 +0300

    gg

--- HotelListing2/Startup.cs
+++ HotelListing2/Startup.cs
@@ -25,15 +25,14 @@ namespace HotelListing2
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration;           
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-
+        {           
             services.AddDbContext<DatabaseContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
 
